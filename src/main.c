88db9a3b458ed14e093a6585cdc2723e15884a66
Viktor <viktorthysen@gmail.com>
2025-05-01 15:39:12 +0200

    made the GUI react

--- src/main.c
+++ src/main.c
@@ -1,14 +1,34 @@
+#define SDL_MAIN_HANDLED
+#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
 #include "controller/startupPhase.h"
 #include "model/playPhaseCommands.h"
 
 //USED FOR TEST (DELETE BEFORE SUBMISSION)
+#include <view/GUI.h>
 #include "model/StartPhaseCommands.h"
 
 
-int main(void) {
 
-//RunPlayPhase();
-    //Lines below used for test (DELETE BEFORE SUBMISSION)
+
+
+
+SDL_AppInit_func (start);
+
+SDL_AppEvent_func(press);
+
+SDL_AppIterate_func(update);
+
+SDL_AppQuit_func(close);
+
+
+
+
+
+
+
+
+
+/*
     LD(NULL);
     InitArray();
     Card* k1 = columnArr[0];
@@ -22,6 +42,6 @@ int main(void) {
     RunStartupPhase();
 
     //MoveTopCards(&columnArr[1],&columnArr[0],5);
-    PrintPlayPhase("ok", "ok");
+    PrintPlayPhase("ok", "ok"); */
 
-}
+
---
+++ src/view/GUI.h
@@ -0,0 +1,12 @@
+//
+// Created by vikto on 01-05-2025.
+//
+
+#ifndef GUI_H
+#define GUI_H
+
+#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+#endif //GUI_H
